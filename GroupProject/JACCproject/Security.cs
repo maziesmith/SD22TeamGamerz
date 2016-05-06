706025644bd1c1cecee60639390fb1d4c2a9678c
Christian Yusi <ckdy_27@yahoo.com>
2016-05-06 14:19:16 -0500

    Delete Files

---
+++ GroupProject/JACCproject/Security.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JACCproject
+{
+    public class Security
+    {
+        public static Client CurrentClient
+        {
+            get
+            {
+                return HttpContext.Current.Session["Client"] == null ? null : (Client)HttpContext.Current.Session["Client"];
+            }
+        }
+        public static void Login(string userName, string password)
+        {
+            HttpContext.Current.Session["Customer"] = Client.Login(userName, password);
+        }
+    }
+}
