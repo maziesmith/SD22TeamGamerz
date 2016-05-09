47dba8004557f0e092a98209b93e08a6805d5e2e
Christian Yusi <ckdy_27@yahoo.com>
2016-05-09 13:27:37 -0500

    Redo the Initial Commit

--- GroupProject/GroupProject/GroupProject/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace GroupProject.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
---
+++ GroupProject/GroupProject/GroupWebProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GroupWebProject.Startup))]
+namespace GroupWebProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
