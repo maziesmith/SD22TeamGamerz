bab8f791a08706333b0361e614259a1beac08b92
Christian Yusi <ckdy_27@yahoo.com>
2016-05-09 16:57:56 -0500

    Completed the Classes

--- GroupProject/GroupProject/GPClassLibrary/Category.cs
+++ GroupProject/GroupProject/GPClassLibrary/Category.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
 
 namespace GPClassLibrary
 {
-    class Category
+    public class Category
     {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        public Category() { }
+
+        public Category(int CategoryID)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["dbGroupProject"].ConnectionString;
+            DAL_Project.DAL d = new DAL_Project.DAL(connStr);
+            d.AddParam("CategoryID", CategoryID);
+            DataSet ds = d.ExecuteProcedure("spGetCategoryByID");
+        }
+
+        public static List<Category> GetAllCategories()
+        {
+            List<Category> resultList = new List<Category>();
+
+            string connStr = ConfigurationManager.ConnectionStrings["dbGroupProject"].ConnectionString;
+            DAL_Project.DAL d = new DAL_Project.DAL(connStr);
+            DataSet ds = d.ExecuteProcedure("spGetCategoryByID");
+
+            foreach (DataRow category in ds.Tables[0].Rows)
+            {
+                Category newCategory = new Category()
+                {
+                    CategoryID = int.Parse(category["CategoryID"].ToString()),
+                    CategoryName = category["Name"].ToString()
+                };
+
+                resultList.Add(newCategory);
+            }
+            return resultList;
+        }
     }
+
 }
