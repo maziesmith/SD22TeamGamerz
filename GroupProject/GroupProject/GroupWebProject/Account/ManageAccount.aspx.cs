07deffb17af29ce3c2c86b02d409a47b797acbfa
Jamie Newman <njamie1997@gmail.com>
2016-05-20 15:13:41 -0500

    Updating accounts is now functional.

--- GroupProject/GroupProject/GroupWebProject/Account/ManageAccount.aspx.cs
+++ GroupProject/GroupProject/GroupWebProject/Account/ManageAccount.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using GPClassLibrary;
 
 namespace GroupWebProject.Account
 {
@@ -11,7 +12,37 @@ namespace GroupWebProject.Account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string userID = Request.QueryString["user"];
+                if (userID != Security.CurrentClient.ClientID.ToString())
+                {
+                    Response.Redirect("Default.aspx");
+                }
+                else
+                {
+                    List<Client> userList = new List<Client>();
+                    Client currentUser = new Client();
+                    
+                    userList = currentUser.GetClient(userID);
+                    lblClientID.Text = userList[0].ClientID.ToString();
+                    lblUserName.Text = userList[0].UserName;
+                    txtFirstName.Text = userList[0].FirstName;
+                    txtLastName.Text = userList[0].LastName;
+                    txtEmail.Text = userList[0].Email;
+                    txtCity.Text = userList[0].City;
+                    txtAddress.Text = userList[0].Address;
+                    txtPostalCode.Text = userList[0].PostalCode;
+                    txtPhoneNumber.Text = userList[0].PhoneNumber;
+                }
+            }
+        }
 
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Client updateClient = new Client();
+            updateClient.UpdateClient(Security.CurrentClient.ClientID.ToString(), txtFirstName.Text, txtLastName.Text, txtAddress.Text, txtCity.Text, txtPostalCode.Text, txtPhoneNumber.Text, lblUserName.Text, Security.CurrentClient.Password, txtEmail.Text, Security.CurrentClient.IsAdmin);
+            Response.Redirect("~/Default.aspx");
         }
     }
 }
